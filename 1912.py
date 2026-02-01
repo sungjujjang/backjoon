e1315c427b1627701e2e6e2e8a8fbd901a32a8e4
sungjujjang <hanbe7220@gmail.com>
2026-02-01 17:57:02 +0900

    0202제출예정

---
+++ 1912.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [0] * n
+dp[0] = a[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i-1] + a[i], a[i])
+
+print(max(dp))
